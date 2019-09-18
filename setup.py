950ea6ad86dae46b17beb3733c660f5858f59429
Jeffrey Mvutu <jeffrey.mvutu@gmail.com>
2019-09-18 10:19:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
-from jmm import version
+# from jmm import version
 
 # Package meta-data.
 NAME = 'jmm'
@@ -20,7 +20,7 @@ URL = 'https://github.com/JeffMv/jmm-util-libs'
 EMAIL = 'jeffrey.mvutu@gmail.com'
 AUTHOR = 'Jeffrey Mvutu Mabilama'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = version.VERSION
+VERSION = "0.1.2.9.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
